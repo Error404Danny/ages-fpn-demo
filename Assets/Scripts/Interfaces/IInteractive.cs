1c4db91c65633323ca66e5776363634520dfc23a
Error404Danny <danny.lopez@loop.colum.edu>
2020-03-01 19:55:05 -0600

    More life towards the project
    Added Raycasting (still working on it), terrain with texture, fixed lights, and made character bigger than before

---
+++ Assets/Scripts/Interfaces/IInteractive.cs
@@ -0,0 +1,7 @@
+﻿/// <summary>
+/// Interface for elements the player can interact with by pressing the Interact Button
+/// </summary>
+public interface IInteractive 
+{
+    void InteractWith();
+}
